4f9998b3bc5f05eefec65eec56aed2185ac8ddd4
Shake <143591158+Shake1227@users.noreply.github.com>
2025-11-05 18:29:25 +0900

    fix

--- src/main/java/shake1227/skincloset/core/ClientEvents.java
+++ src/main/java/shake1227/skincloset/core/ClientEvents.java
@@ -11,17 +11,18 @@ import shake1227.skincloset.client.gui.SkinClosetScreen;
 import shake1227.skincloset.network.C2SChangeSkinPacket;
 import shake1227.skincloset.network.PacketRegistry;
 import shake1227.skincloset.skin.LastSkinCache;
+
 public class ClientEvents {
 
     @SubscribeEvent
-    public static void onKeyInput(InputEvent.Key event) {
+    public void onKeyInput(InputEvent.Key event) {
         if (Keybinds.OPEN_GUI_KEY.consumeClick()) {
             Minecraft.getInstance().setScreen(new SkinClosetScreen());
         }
     }
 
     @SubscribeEvent
-    public static void onPlayerLogin(ClientPlayerNetworkEvent.LoggingIn event) {
+    public void onPlayerLogin(ClientPlayerNetworkEvent.LoggingIn event) {
         LastSkinCache.LastSkinData lastSkin = LastSkinCache.load();
         if (lastSkin != null) {
             SkinCloset.LOGGER.info("Applying persistent skin from last session.");
